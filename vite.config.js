f3e50bd23dedb8afd7eca85d582ce90c8a2148df
Ravi Shankar Kumar <rskumar.hit@gmail.com>
2025-04-29 03:20:02 +0530

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
+  },
+  base: "/list-creation-app/",
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
