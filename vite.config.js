cd3a70931b86d5125190550e70929cc7248ffe8f
Ravi Shankar Kumar <rskumar.hit@gmail.com>
2025-04-29 17:17:35 +0530

    Update dist folder and vite config for proper deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/list-creation-app/" : "/",
+  base: "./",
   css: {
     postcss: "./postcss.config.js",
   },
@@ -24,5 +24,6 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    host: true,
   },
 });
